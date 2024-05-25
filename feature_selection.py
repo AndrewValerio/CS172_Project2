c58bd3c29e6a395ba6b42a536166fe7b319ef7bc
ShreyGK6 <146229303+ShreyGK6@users.noreply.github.com>
2024-05-25 16:04:05 -0700

    created nearestNeighbor (rough draft)

--- feature_selection.py
+++ feature_selection.py
@@ -1,4 +1,5 @@
 import random
+import math
 
 def evaluate(features):
     # Replace this with part 2 stuff
@@ -29,3 +30,22 @@ def loadData(self, filename = "small-test-dataset.txt"):
                 self.dataVals[classVal] = instances
 
     file.close()
+
+def nearestNeighborClassifier(dataset, point, feature_subset, number_of_instances):
+	nearest_neighbor = 0
+	shortest_distance = float('inf')
+	for i in range(number_of_instances):
+		if point == i:
+			pass
+		else:
+			distance = 0
+			for j in range(len(feature_subset)):
+				distance = distance + pow((dataset[i][feature_subset[j]] - dataset[point][feature_subset[j]]), 2)
+
+			distance = math.sqrt(distance)
+
+			if distance < shortest_distance:
+				nearest_neighbor = i 
+				shortest_distance = distance
+
+	return nearest_neighbor
