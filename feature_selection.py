da3dd0834bd11cc5e51bbf00cd7fda401bd5aca2
AndrewValerio <andrewvalerio1396@gmail.com>
2024-05-25 15:10:02 -0700

    added starting functions

--- feature_selection.py
+++ feature_selection.py
@@ -1,5 +1,13 @@
 import random
+import main
 
 def evaluate(features):
     # Replace this with part 2 stuff
     return random.random()
+
+def forward_selection(total_features):
+    return
+
+
+def backward_elimination(total_features):
+    return 
