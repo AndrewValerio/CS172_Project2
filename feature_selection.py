bfcd033a57e97b8d01be084e2fd5c03734800fcd
ShreyGK6 <146229303+ShreyGK6@users.noreply.github.com>
2024-05-26 21:01:00 -0700

    added the two functions forward and backward

--- feature_selection.py
+++ feature_selection.py
@@ -3,15 +3,61 @@ import math
 import numpy as np
 
 def evaluate(features):
-    # Replace this with part 2 stuff
-    return random.random()
+    return random.uniform(0, 1)
 
 def forward_selection(total_features):
-    return
+    running_features = []
+    remaining = list(range(1, total_features + 1))
 
+    print("Beginning search.")
+
+    while remaining:
+        best_feature = None
+        max_score = -1
+
+        for feature in remaining:
+            #print("feature is: " + str(feature))
+            current = running_features + [feature]
+            #print("curr feature is: " + str(current_features))
+            score = evaluate(current) * 100
+            print(f"Using feature(s) {set(current)} accuracy is {score:.1f}%")
+
+            if score > max_score:
+                max_score = score
+                best_feature = feature
+
+        if best_feature is not None:
+            running_features.append(best_feature)
+            remaining.remove(best_feature)
+            print(f"Feature set {set(running_features)} was best, accuracy is {max_score:.1f}%")
+    print("\nFinal features:", running_features)
+    return None
 
 def backward_elimination(total_features):
-    return 
+    current_set = set(range(1, total_features + 1))
+    best_featureset = current_set.copy()
+    best_setscore = -1
+    while current_set:
+        highest_score = -1
+        worst_feature = None
+        for feature in current_set:
+            temp_set = current_set - {feature}
+            score = evaluate(temp_set) * 100
+            print(f"Using feature(s) {set(temp_set)} accuracy is {score:.1f}%")
+            if score > highest_score:
+                worst_feature = feature
+                highest_score = score
+        if worst_feature:
+            current_set.remove(worst_feature)
+            if score > best_setscore:
+                best_setscore = score
+                best_featureset = current_set.copy()
+        else:
+            break
+        print(f"Feature set {set(current_set)} was best, accuracy is {highest_score:.1f}%")   
+    print(f"Finished search!! The best feature subset is {set(best_featureset)} which has an accuracy of {best_setscore:.1f}%")
+
+    return best_featureset, best_setscore
 
 def loadData(self, filename = "small-test-dataset.txt"):
     self.dataVals = {} # dictionary that will hold the data
@@ -29,7 +75,6 @@ def loadData(self, filename = "small-test-dataset.txt"):
                 instances = self.dataVals.get(classVal, [])
                 instances.append(float(j)) # python float() converts IEEE to double automatically
                 self.dataVals[classVal] = instances
-
     file.close()
 
 class Classifier_Class():
